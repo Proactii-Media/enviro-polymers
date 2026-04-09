b53302da9712566af4fbf85aaf24d5f5f69b66d3
Proactii <hello@proactii.com>
2026-04-09 16:55:44 +0530

    Update page.tsx

--- app/(root)/clients/page.tsx
+++ app/(root)/clients/page.tsx
@@ -103,7 +103,7 @@ export default function ClientsPage() {
     visible: {
       opacity: 1,
       y: 0,
-      transition: { duration: 0.8, ease: "easeOut" },
+      transition: { duration: 0.8, ease: "easeOut" as const },
     },
   };
 
